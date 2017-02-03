47c6c483e5fe5da1388148173653b3832b4e1976
Heather Turner <ht@heatherturner.net>
2017-02-03 13:50:12 +0000

    configure *after* initializing repo

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -7,15 +7,14 @@ GH_REPO="@github.com/hturner/conf-management-proposal.git"
 
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
-# configure git on travis for this repo; use dummy identity
-git config user.name "Travis CI"
-git config user.email "travis@travis.ci"
-
 R CMD BATCH 'ghgenerate.R'
 cp ghgenerate.Rout out
 
+# initialize repo from out on travis; configure with dummy identity
 cd out
 git init
+git config user.name "Travis CI"
+git config user.email "travis@travis.ci"
 git add .
 git commit -m "deployed to github pages"
 git push --force --quiet $FULL_REPO master:gh-pages
